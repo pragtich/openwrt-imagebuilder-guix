e9f0535aa27049bf9e6036a64ff2d01c80512703
Joris Pragt <pragtich@gmail.com>
2026-04-08 21:23:09 +0200

    Added first go at downloading the imagebuilder

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+REL="25.12.2"
+ARCH="x86"
+VARIANT="64"
+IMGBLDR_FN="openwrt-imagebuilder-${REL}-${ARCH}-${VARIANT}.Linux-${ARCH}_${VARIANT}.tar.zst"
+IMGBLDR_URL="https://downloads.openwrt.org/releases/${REL}/targets/${ARCH}/${VARIANT}/${IMGBLDR_FN}"
+
+wget $IMGBLDR_URL
+
 guix shell --container --manifest=manifest.scm
+
+
+#TODO: switch to package structure?

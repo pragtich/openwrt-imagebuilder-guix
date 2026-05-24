9ef8d9e0ef912c020895cf2138dcb7c208fbec48
Joris Pragt <pragtich@gmail.com>
2026-05-24 15:50:58 +0200

    Add nocontainer script

---
+++ imagebuilder-nocontainer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REL="25.12.3"
+ARCH="x86"
+VARIANT="64"
+IMGBLDR_FN="openwrt-imagebuilder-${REL}-${ARCH}-${VARIANT}.Linux-${ARCH}_${VARIANT}.tar.zst"
+IMGBLDR_URL="https://downloads.openwrt.org/releases/${REL}/targets/${ARCH}/${VARIANT}/${IMGBLDR_FN}"
+
+PROFILE="generic"		# make info: the only option for x86/64
+FILES="files"		
+# ROOTFS_PARTSIZE runs into limitations due to Docker limits. Probably avoidable, but let's try resizing first
+#ROOTFS_PARTSIZE=143 	# Partition size in MB; 14.7G presently 14*1024=14336MB
+
+mkdir -p ${PWD}/tmp
+
+wget -N $IMGBLDR_URL
+
+guix shell --container --manifest=manifest.scm --network --emulate-fhs --share=${PWD}/tmp --share=/tmp -- bash -i -s <<EOF
+echo "Reading packages"
+declare -a PACKAGES		# space separated list; -package to exclude; auto dependencies
+source packages
+
+export MAKE_TMPDIR ${PWD}/tmp
+export TMPDIR ${PWD}/tmp
+
+echo "Decompressing"
+tar --zstd  -xvf $IMGBLDR_FN
+cd $(basename $IMGBLDR_FN .tar.zst)
+
+echo "Preventative clean"
+#make clean
+mkdir tmp
+
+echo "Starting build"
+
+#make image PROFILE="$PROFILE" FILES="$MYFILES" PACKAGES="${PACKAGES[*]}"
+make image PROFILE="$PROFILE" FILES="pragtich/files" PACKAGES="${PACKAGES[*]}" 
+
+echo "Done."
+
+EOF
+
+
+#TODO: switch to package structure?

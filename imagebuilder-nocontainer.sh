52a84829461e5c6348f7ed39f11e0ddc34edf37d
Joris Pragt <pragtich@gmail.com>
2026-06-09 12:28:30 +0200

    More cleanup

--- imagebuilder-nocontainer.sh
+++ imagebuilder-nocontainer.sh
@@ -7,21 +7,19 @@ export IMGBLDR_FN="openwrt-imagebuilder-${REL}-${ARCH}-${VARIANT}.Linux-${ARCH}_
 export IMGBLDR_URL="https://downloads.openwrt.org/releases/${REL}/targets/${ARCH}/${VARIANT}/${IMGBLDR_FN}"
 export IMGBLDR_PROFILE="generic"		# make info: the only option for x86/64
 
-wget -N $IMGBLDR_URL
-echo "Filename: $IMGBLDR_FN"
-guix shell --pure --manifest=manifest.scm --network --share=${PWD}/tmp --share=/tmp --preserve='^IMGBLDR' -- bash -s <<< $(cat packages ; cat <<'EOF' )
-echo "Package list:"
+echo "Selected additional packages:"
 echo "${PACKAGES[*]}"
 
+echo "Downloading release."
+wget -N $IMGBLDR_URL
+guix shell --pure --manifest=manifest.scm --network --preserve='^IMGBLDR' -- bash -s <<< $(cat packages ; cat <<'EOF' )
 export MYFILES="../files"
-echo "Filename: $IMGBLDR_FN"
 echo "Decompressing"
 tar --zstd  -xvf $IMGBLDR_FN
 cd $(basename $IMGBLDR_FN .tar.zst)
 
 echo "Preventative clean"
 make clean
-mkdir -p tmp
 
 echo "Starting build"
 make image PROFILE="$IMGBLDR_PROFILE" FILES="$MYFILES" PACKAGES="${PACKAGES[*]}"
